a157bb9ae6e04f22e9ecbe2c1215bc35f948d634
Purwa Sadr <purwasadr@gmail.com>
2021-12-04 11:29:23 +0700

    change name edit.blade.php to create.blade.php and change something

---
+++ resources/views/product/create.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="row">
+    <form method="post" action="/dashboard/posts" class="col-md-7 mb-5" enctype="multipart/form-data">
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                required autofocus value="{{ old('title') }}">
+            @error('title')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="slug" class="form-label">Slug</label>
+            <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required
+                value="{{ old('slug') }}">
+            @error('slug')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                required value="{{ old('price') }}">
+            @error('price')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Product Image</label>
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image"
+                onchange="previewImage()">
+            @error('image')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="body" class="form-label">Body</label>
+            @error('body')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
+            <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+            <trix-editor input="body"></trix-editor>
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
+</div>
+@endsection
