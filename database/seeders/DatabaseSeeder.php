28bfd2ce30c882f624fd0a0b758fb7231df928b7
Purwa Sadr <purwasadr@gmail.com>
2021-12-05 09:50:00 +0700

    Add Product and User seeder create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
         Product::create([
-            'name' => 'Sepatu',
-            ''
+            'title' => 'Sepatu',
+            'price' => '400000',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
+            'image' => null,
+            'user_id' => 1
+        ]);
+
+        Product::create([
+            'title' => 'Hp Kotak Urip',
+            'price' => '24000000',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
+            'image' => null,
+            'user_id' => 1
+        ]);
+
+        Product::create([
+            'title' => 'Intel 1300K',
+            'price' => '11400000',
+            'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
+            'image' => null,
+            'user_id' => 1
         ]);
 
         // Product::factory(15)->create();
