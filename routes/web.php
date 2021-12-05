94dfca5fd3267e56894bc72a307d8de6785431ae
Purwa Sadr <purwasadr@gmail.com>
2021-12-05 09:52:55 +0700

    add Route : * Get '/login' * Post '/login' * Get '/signup' * Post '/signup' * Post '/logout' * Get '/create' * Get '/seller'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignUpController;
 use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +20,25 @@ Route::get('/', function () {
     return view('home', [
         'title' => 'Home',
         'products' => Product::all()
+    ]);
+});
+
+Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+Route::get('/signup', [SignUpController::class, 'index']);
+Route::post('/signup', [SignUpController::class, 'store']);
+
+Route::post('/logout', [LoginController::class, 'logout']);
+
+Route::get('/create', function () {
+    return view('product.create', [
+        'title' => 'Edit',
+    ]);
+});
+
+Route::get('/seller', function () {
+    return view('seller.layouts.main', [
+        'title' => 'Seller Home',
     ]);
 });
