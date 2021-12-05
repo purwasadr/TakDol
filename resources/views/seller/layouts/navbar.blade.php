57aa6460310228e2156731355d35e81891ae0f61
Purwa Sadr <purwasadr@gmail.com>
2021-12-05 20:29:56 +0700

    add main, navbar, sidebar view

---
+++ resources/views/seller/layouts/navbar.blade.php
@@ -0,0 +1,51 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">TakDol</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="/seller">Mulai Jual</a>
+                </li>
+            </ul>
+            @auth
+            <div class="navbar-nav">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Welcome back, {{ auth()->user()->name }}
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i>
+                                Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li>
+                            <form action="/logout" method="post">
+                                @csrf
+                                <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-in-right"></i>
+                                    Logout</button>
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </div>
+            @else
+            <ul class="navbar-nav">
+                <li class="nav-item mt-2 mt-lg-0 lg">
+                    <a class="btn btn-primary ms-lg-3" aria-current="page" href="/login">Login</a>
+                </li>
+                <li class="nav-item mt-2 mt-lg-0">
+                    <a class="btn btn-primary ms-lg-2" aria-current="page" href="/signup">SignUp</a>
+                </li>
+            </ul>
+            @endauth
+
+        </div>
+    </div>
+</nav>
---
+++ resources/views/seller/layouts/sidebar.blade.php
@@ -0,0 +1,18 @@
+<nav id="sidebarMenu" class="col-lg-2 position-fixed d-md-block collapse">
+    <div class="p-3 position-sticky bg-white shadow-sm vh-100">
+        <ul class="nav flex-column nav-pills">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">My Product</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Much</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</nav>
