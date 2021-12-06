3be165b5f2ef8979895ec277009398619f7222ab
Purwa Sadr <purwasadr@gmail.com>
2021-12-06 19:12:47 +0700

    Change margin and change tag : * change search input group margin button to mb-5 * Category margin button change to mb-4 * Change tag h5 in card body to small * Change to real path to image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-lg-7 mb-3">
+    <div class="col-lg-7 mb-5">
         <div class="input-group">
             <input type="text" class="form-control" placeholder="Search..." name="search">
             <button class="btn btn-primary" type="submit">Search</button>
@@ -10,7 +10,7 @@
     </div>
 </div>
 <div class="row justify-content-center">
-    <div class="col-md-4 mb-3">
+    <div class="col-md-4 mb-4">
         <ul class="nav nav-pills">
             <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">All</a>
@@ -32,12 +32,12 @@
     <div class="col-md-2 mb-3">
         <div class="card h-100">
             <div class="ratio ratio-1x1">
-                <img src="{{ asset('storage/img_product/mikasa_ackerman_shingeki_no_kyojin_swords_guns_102914_1920x1080.jpg') }}"
-                    class="card-img-top rat" style="object-fit: cover;" alt="Mikasa">
+                <img src="{{ asset('storage' . $product->image) }}" class="card-img-top rat" style="object-fit: cover;"
+                    alt="{{ $product->image }}">
             </div>
             <div class="card-body">
-                <h5 class="card-title">{{ $product->name }}</h5>
-                <p>Rp. {{ $product->price }}</p>
+                <small class="card-title">{{ $product->title }}</small>
+                <h6 class="mb-0">Rp. {{ $product->price }}</h6>
                 <a href="#" class="stretched-link"></a>
             </div>
         </div>
