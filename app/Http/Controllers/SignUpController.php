7c6e1ff1a301b86a962e7ec4d81ed84b8c448c1a
Purwa Sadr <purwasadr@gmail.com>
2021-12-06 19:08:50 +0700

    Add store funtion and add public modifier

--- app/Http/Controllers/SignUpController.php
+++ app/Http/Controllers/SignUpController.php
@@ -2,14 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class SignUpController extends Controller
 {
-    function index(Request $request)
+    public function index()
     {
         return view('signup.index', [
             'title' => 'Sign Up'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'username' => 'required|min:3|max:255|unique:users',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:5|max:255'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        User::create($validatedData);
+
+        return redirect('/login')->with('success', "Registration successfull! Please login");
     }
 }
