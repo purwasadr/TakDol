91d5f1abb8bd294f8059dc6e8f8aa8b98527920b
Purwa Sadr <purwasadr@gmail.com>
2021-12-12 07:19:28 +0700

    Change folder name index blade to registration

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -6,7 +6,7 @@
     <div class="col-lg-5">
         <main class="form-registration">
             <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
-            <form action="/signup" method="post">
+            <form action="/register" method="post">
                 @csrf
                 <div class="form-floating">
                     <input type="text" class="form-control rounded-top  @error('name') is-invalid @enderror" name="name"
