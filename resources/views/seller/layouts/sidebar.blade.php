d1aae65ac4b3ac5b3f6324f2f471379ef51180f2
Purwa Sadr <purwasadr@gmail.com>
2021-12-20 09:59:09 +0700

    Remove unused nav-item and change href

--- resources/views/seller/layouts/sidebar.blade.php
+++ resources/views/seller/layouts/sidebar.blade.php
@@ -1,17 +1,11 @@
-<nav id="sidebarMenu" class="col-lg-2 position-fixed d-md-block collapse">
+<nav id="sidebarMenu" class="col-lg-2 position-fixed d-lg-block collapse">
     <div class="p-3 position-sticky bg-white shadow-sm vh-100">
-        <ul class="nav flex-column nav-pills">
+        <ul class="d-flex flex-column nav nav-pills">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">My Product</a>
+                <a class="nav-link active" aria-current="page" href="/seller/myproducts">My Product</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Much</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
             </li>
         </ul>
     </div>
