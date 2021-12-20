c4a45f85ae462fc3293c0455474c971c77f2ee2e
Purwa Sadr <purwasadr@gmail.com>
2021-12-20 10:01:49 +0700

    Add category_id in Product and Create Category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
@@ -16,13 +17,30 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(5)->create();
 
+        Category::create([
+            'name' => 'Shoes',
+            'slug' => 'shoes'
+        ]);
+
+        Category::create([
+            'name' => 'Electronic',
+            'slug' => 'electronic'
+        ]);
+
+        Category::create([
+            'name' => 'Clothes',
+            'slug' => 'clothes'
+        ]);
+
+
         Product::create([
             'title' => 'Sepatu',
             'slug' => 'sepatu',
             'price' => '400000',
             'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
             'image' => 'img_products/dummy-shoes.jpg',
-            'user_id' => 1
+            'user_id' => 1,
+            'category_id' => 1
         ]);
 
         Product::create([
@@ -31,7 +49,8 @@ class DatabaseSeeder extends Seeder
             'price' => '24000000',
             'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
             'image' => 'img_products/dummy-iphone-11.jpg',
-            'user_id' => 1
+            'user_id' => 1,
+            'category_id' => 2
         ]);
 
         Product::create([
@@ -40,7 +59,8 @@ class DatabaseSeeder extends Seeder
             'price' => '11400000',
             'description' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nemo aut perspiciatis ad repellat tempora nisi doloremque veniam minus, nam consectetur quam magni illum. Ullam quod dolorum nesciunt culpa cum?',
             'image' => 'img_products/dummy-intel-i5.jpg',
-            'user_id' => 1
+            'user_id' => 1,
+            'category_id' => 2
         ]);
 
         // Product::factory(15)->create();
