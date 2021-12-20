91f70237f12d8f8b41584f3a50d0beb489ba0118
Purwa Sadr <purwasadr@gmail.com>
2021-12-20 09:59:42 +0700

    Add necessary code

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,11 +2,22 @@
 
 @section('container')
 <div class="row">
-    <div class="col-md-5">
-        <img src="{{  asset('storage/' . $product->image) }}" alt="">
+    <div class="col-md-4">
+        <div class="ratio ratio-1x1">
+            <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" style="object-fit: cover;"
+                alt="{{ $product->image }}">
+        </div>
     </div>
-    <div class="col-md-12">
-        <h2>{{ $product->title }}</h2>
+    <div class="col-md-8">
+        <h4>{{ $product->title }}</h4>
+        <h3 class="mt-2">Rp. {{ number_format($product->price, 0, ",", ".") }}</h3>
+
+        <div class="mt-2 d-inline-block mb-2">
+            <a class="btn btn-outline-primary mt-2" href="/buy">Tambahkan ke keranjang</a>
+            <a class="btn btn-primary mt-2" href="/buy">Beli Sekarang</a>
+        </div>
     </div>
 </div>
+<h6 class="mt-5">Description</h6>
+<p style="white-space: pre-line">{{ $product->description }}</p>
 @endsection
