feefe675be02faedec274edf82de24679b3a919f
Purwa Sadr <purwasadr@gmail.com>
2021-12-23 09:30:16 +0700

    Add myproduct.edit blade

---
+++ resources/views/seller/myproducts/edit.blade.php
@@ -0,0 +1,82 @@
+@extends('seller.layouts.main')
+
+@section('container')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Edit Product</h1>
+</div>
+<div class="col-lg-8">
+    <form method="post" action="/seller/myproducts" class="mb-5" enctype="multipart/form-data">
+        @method('put')
+        @csrf
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+                required autofocus value="{{ old('title', $product->title) }}">
+            @error('title')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="slug" class="form-label">Slug</label>
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="slug" name="slug" required
+                autofocus value="{{ old('slug', $product->slug) }}">
+            @error('slug')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price</label>
+            <input type="number" class="form-control @error('title') is-invalid @enderror" id="price" name="price"
+                required autofocus value="{{ old('price', $product->price) }}">
+            @error('price')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="category" class="form-label">Category</label>
+            <select class="form-select" name="category_id">
+                @foreach ($categories as $category)
+                @if (old('category_id', $product->category_id) == $category->id)
+                <option value={{ $category->id }} selected>{{ $category->name }}</option>
+                @else
+                <option value={{ $category->id }}>{{ $category->name }}</option>
+                @endif
+
+                @endforeach
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Product Image</label>
+            <input type="hidden" name="oldImage" value="{{ $product->image }}">
+            @if ($product->image)
+            <img src="{{ asset('storage/' . $product->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block">
+            @else
+            <img class="img-preview img-fluid mb-3 col-sm-5">
+            @endif
+            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image"
+                onchange="previewImage()">
+            @error('image')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description</label>
+            <textarea id="description" name="description" value="{{ old('description', $product->description) }}"
+                class="form-control" aria-label="With textarea" rows="4"></textarea>
+            @error('body')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
+        </div>
+        <button type="submit" class="btn btn-primary">Create Post</button>
+    </form>
+</div>
+<script src="/js/seller.js"></script>
+@endsection
