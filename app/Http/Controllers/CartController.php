03a53e6c4dceec6c1a8b93357e8d6319b020de91
Purwa Sadr <purwasadr@gmail.com>
2021-12-29 16:03:50 +0700

    Add cart Controller

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function index(Product $product)
+    {
+        return view('cart.index', [
+            'title' => 'Cart',
+            'carts' => Cart::where('user_id', Auth::user()->id)->get()
+        ]);
+    }
+
+    public function checkout(Request $request)
+    {
+        $data = array();
+        foreach ($request->cart_check as $product_id) {
+            array_push($data, [
+                'user_id' => Auth::user()->id,
+                'product_id' => $product_id
+            ]);
+        }
+
+        Cart::whereIn('product_id', $request->cart_check)->delete();
+        Order::insert($data);
+
+        return redirect('/cart');
+    }
+}
