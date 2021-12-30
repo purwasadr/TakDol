2105cfd16cc356421d70df9870ebe85a7279f4c3
Purwa Sadr <purwasadr@gmail.com>
2021-12-30 10:58:14 +0700

    Add active selection

--- resources/views/seller/layouts/sidebar.blade.php
+++ resources/views/seller/layouts/sidebar.blade.php
@@ -2,10 +2,12 @@
     <div class="p-3 position-sticky">
         <ul class="flex-column nav nav-pills">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="/seller/myproducts">My Product</a>
+                <a class="nav-link {{ Request::is('seller/myproducts') ? 'active' : '' }}" aria-current="page"
+                    href="/seller/myproducts">My Product</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Much</a>
+                <a class="nav-link {{ Request::is('seller/myorders') ? 'active' : '' }}" href="/seller/myorders">My
+                    Orders</a>
             </li>
         </ul>
     </div>
