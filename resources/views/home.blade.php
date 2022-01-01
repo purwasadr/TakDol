33bdfd2d3e18ba0f21c85fe6953f6edce694afe3
Purwa Sadr <purwasadr@gmail.com>
2022-01-01 11:57:08 +0700

    Remove search input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,6 @@
 
 @section('container')
 <div class="row justify-content-center">
-    <div class="col-lg-7 mb-5">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search..." name="search">
-            <button class="btn btn-primary" type="submit">Search</button>
-        </div>
-    </div>
-</div>
-<div class="row justify-content-center">
     <div class="col-md-4 mb-4">
         <ul class="nav nav-pills">
             @if (!request('category'))
@@ -34,7 +26,7 @@
 <div class="row">
     @foreach ($products as $product)
     <div class="col-md-2 mb-3">
-        <div class="card">
+        <div class="card h-100">
             <div class="ratio ratio-1x1">
                 <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" style="object-fit: cover;"
                     alt="{{ $product->image }}">
