b17de7705f561e45dd12eb156a426a2bcd5a014c
Purwa Sadr <purwasadr@gmail.com>
2022-01-01 11:53:02 +0700

    Add HomeController

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function addToCart(Product $product)
+    {
+        Cart::create([
+            'user_id' => Auth::user()->id,
+            'product_id' => $product->id
+        ]);
+
+        return redirect('/show/' . $product->slug);
+    }
+}
