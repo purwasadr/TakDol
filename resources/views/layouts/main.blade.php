c5a29dd4f58df3c75581ade39612458b5d4d9fd5
Purwa Sadr <purwasadr@gmail.com>
2022-01-02 14:25:50 +0700

    Comment font face and add inter css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,17 +16,44 @@
     {{-- My Style --}}
     <link rel="stylesheet" href="/css/style.css">
 
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+
     <title>{{ $title }}</title>
+
+    {{--
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet"> --}}
+    <style>
+        /* @font-face {
+            font-family: 'Inter Medium';
+            src: url('{{ public_path('font/Inter-Medium.ttf') }}');
+        }
+
+        @font-face {
+            font-family: 'Inter var';
+            font-style: normal;
+            font-weight: 100 900;
+            font-display: ;
+            src: url('{{ public_path('font/Inter-var.ttf') }}');
+
+        } */
+
+        body {
+            font-family: 'Inter', Ubuntu, Geneva, Verdana, sans-serif;
+            font-weight: 500;
+        }
+    </style>
 </head>
 
-<body>
+<body style="height: 1000px">
     @include('partials.navbar')
 
     <div class="container mt-4 mb-4">
         @yield('container')
     </div>
 
-    @include('partials.footer')
+    {{-- @include('partials.footer') --}}
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
