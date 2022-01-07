6f506ccbc30e297d3e388f1b4ca868ec8c182599
Purwa Sadr <purwasadr@gmail.com>
2022-01-07 10:13:48 +0700

    change : location my style

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -14,7 +14,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
 
     {{-- My Style --}}
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/app.css">
 
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
