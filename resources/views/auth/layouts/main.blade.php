a89b9a78486397cdfb452260d4252d0a76a9d49f
Purwa Sadr <purwasadr@gmail.com>
2022-01-08 08:44:57 +0700

    Add main blade in 'auth.layouts'

---
+++ resources/views/auth/layouts/main.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    @include('partials.head')
+</head>
+
+<body>
+
+    <div class="container mt-5 mb-5">
+        @yield('container')
+    </div>
+
+    @include('partials.script')
+</body>
+
+</html>
