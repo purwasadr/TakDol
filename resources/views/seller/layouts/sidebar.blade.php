5b089ad143864edfda2053154055e09eb4f4f865
Purwa Sadr <purwasadr@gmail.com>
2022-01-08 08:49:15 +0700

    add : '*' in Request::is path

--- resources/views/seller/layouts/sidebar.blade.php
+++ resources/views/seller/layouts/sidebar.blade.php
@@ -2,11 +2,11 @@
     <div class="p-3 position-sticky">
         <ul class="flex-column nav nav-pills">
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('seller/myproducts') ? 'active' : '' }}" aria-current="page"
+                <a class="nav-link {{ Request::is('seller/myproducts*') ? 'active' : '' }}" aria-current="page"
                     href="/seller/myproducts">My Product</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link {{ Request::is('seller/myorders') ? 'active' : '' }}" href="/seller/myorders">My
+                <a class="nav-link {{ Request::is('seller/myorders*') ? 'active' : '' }}" href="/seller/myorders">My
                     Orders</a>
             </li>
         </ul>
