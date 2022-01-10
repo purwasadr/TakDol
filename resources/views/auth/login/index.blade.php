92e9c666554b1a82c1a617eab3a399e690407266
Purwa Sadr <purwasadr@gmail.com>
2022-01-10 20:14:27 +0700

    change : extends path and change outer div class

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -1,8 +1,8 @@
-@extends('layouts.main')
+@extends('auth.layouts.main')
 
 @section('container')
 
-<div class="row justify-content-center">
+<div class="row justify-content-center col-12">
     <div class="col-md-4">
         @if (session()->has('success'))
         <div class="alert alert-success alert-dismissible fade show" role="alert">
