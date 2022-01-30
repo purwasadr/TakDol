da3a5794b9685205a7a5861a1d6baff1fe0eef59
Purwa Sadr <purwasadr@gmail.com>
2022-01-30 18:10:32 +0700

    Fix type input, Change image input style and Add stock input

--- resources/views/seller/myproducts/create.blade.php
+++ resources/views/seller/myproducts/create.blade.php
@@ -1,8 +1,8 @@
 @extends('seller.layouts.main')
 
 @section('container')
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Add Product</h1>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+    <h4>Add Product</h4>
 </div>
 <div class="col-lg-8">
     <form method="post" action="/seller/myproducts" class="mb-5" enctype="multipart/form-data">
@@ -10,7 +10,7 @@
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
-                required autofocus value="{{ old('title') }}">
+                autofocus value="{{ old('title') }}">
             @error('title')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -19,8 +19,8 @@
         </div>
         <div class="mb-3">
             <label for="slug" class="form-label">Slug</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror" id="slug" name="slug" required
-                autofocus value="{{ old('slug') }}">
+            <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required
+                value="{{ old('slug') }}">
             @error('slug')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -29,9 +29,19 @@
         </div>
         <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="number" class="form-control @error('title') is-invalid @enderror" id="price" name="price"
-                required autofocus value="{{ old('price') }}">
+            <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" name="price"
+                required value="{{ old('price') }}">
             @error('price')
+            <div class="invalid-feedback">
+                {{ $message }}
+            </div>
+            @enderror
+        </div>
+        <div class="mb-3">
+            <label for="stock" class="form-label">Stock</label>
+            <input type="number" class="form-control @error('stock') is-invalid @enderror" id="stock" name="stock"
+                required value="{{ old('stock') }}">
+            @error('stock')
             <div class="invalid-feedback">
                 {{ $message }}
             </div>
@@ -50,10 +60,17 @@
             </select>
         </div>
         <div class="mb-3">
-            <label for="image" class="form-label">Product Image</label>
-            <img class="img-preview img-fluid mb-3 col-sm-5">
-            <input class="form-control @error('image') is-invalid @enderror" type="file" id="image" name="image"
-                onchange="previewImage()">
+            <label class="form-label">Product Image</label>
+            <div id="div-img-product" class="ratio ratio-1x1 w-25 mb-3 bg-plus-icon">
+                <span class="border border-info">
+                </span>
+                <img id="img-product" class="img-preview img-fluid" style="object-fit: contain">
+
+                <label for="image" class="d-block">
+                    <input type="file" class="@error('image') is-invalid @enderror" id="image" name="image"
+                        value="{{ old('image') }}" accept="image/*" onchange="previewImage()" hidden>
+                </label>
+            </div>
             @error('image')
             <div class="invalid-feedback">
                 {{ $message }}
@@ -63,7 +80,7 @@
         <div class="mb-3">
             <label for="description" class="form-label">Description</label>
             <textarea id="description" name="description" class="form-control" aria-label="With textarea"
-                rows="10">{{ old('title') }}</textarea>
+                rows="10">{{ old('description') }}</textarea>
             @error('description')
             <p class="text-danger">{{ $message }}</p>
             @enderror
