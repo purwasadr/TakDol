12c9dc3ce09ff93ad2b7c67bb56414dd0c705227
Purwa Sadr <purwasadr@gmail.com>
2022-01-31 09:03:32 +0700

    Remove unused code

--- resources/views/seller/layouts/main.blade.php
+++ resources/views/seller/layouts/main.blade.php
@@ -7,14 +7,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
-
-    {{-- Seller Style --}}
-    <link rel="stylesheet" href="/css/seller.css">
 
     <title>{{ $title }}</title>
 
@@ -27,6 +23,12 @@
         body {
             font-family: 'Inter Medium', Ubuntu, Geneva, Verdana, sans-serif
         }
+
+        @media (min-width: 768px) {
+            .offcanvas-backdrop {
+                display: none !important;
+            }
+        }
     </style>
 </head>
 
@@ -37,15 +39,13 @@
         <div class="row">
             @include('seller.layouts.sidebar')
 
-            <div class="col-lg-10 ms-lg-auto">
+            <div class="col-md-9 col-lg-10 ms-md-auto px-3">
                 @yield('container')
             </div>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
-    </script>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
