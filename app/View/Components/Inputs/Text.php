5c8e09af376b7d9ba24d39607168c83cf433019f
Purwa Sadr <purwasadr@gmail.com>
2022-02-02 11:34:05 +0700

    Add required attribute

--- app/View/Components/Inputs/Text.php
+++ app/View/Components/Inputs/Text.php
@@ -11,18 +11,20 @@ class Text extends Component
     public $label;
     public $id;
     public $autofocus;
+    public $required;
 
     /**
      * Create a new component instance.
      *
      * @return void
      */
-    public function __construct($name, $label, $id = null, $autofocus = false)
+    public function __construct($name, $label, $id = null, $autofocus = false, $required = false)
     {
         $this->name = $name;
         $this->label = $label;
         $this->id = $id;
         $this->autofocus = $autofocus;
+        $this->required = $required;
     }
 
     /**
