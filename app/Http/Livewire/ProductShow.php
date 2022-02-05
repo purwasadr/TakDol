e9c12409bbdfdf4caba4d78903e29f752644b481
Purwa Sadr <purwasadr@gmail.com>
2022-02-05 08:52:21 +0700

    Add addToCart method

--- app/Http/Livewire/ProductShow.php
+++ app/Http/Livewire/ProductShow.php
@@ -2,19 +2,41 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Cart;
 use App\Models\Product;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ProductShow extends Component
 {
+    use AuthorizesRequests;
+
     public $product;
+    public $count = 0;
 
     public function render()
     {
         return view('livewire.product-show');
     }
 
-    private function getProduct()
+    public function addToCart($id, $count)
     {
+        if (Auth::guest()) {
+            return redirect()->route('login.form');
+        }
+
+        if (Cart::where('product_id', $id)->where('user_id', Auth::user()->id)->exists()) {
+            session()->flash('error', 'You add same product');
+            return;
+        }
+
+
+
+        Cart::create([
+            'user_id' => Auth::user()->id,
+            'product_id' => $id,
+            'count' => $count
+        ]);
     }
 }
