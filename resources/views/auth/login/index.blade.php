a183c917f4871acb4b3d6f16f6ae75883033c32b
Purwa Sadr <purwasadr@gmail.com>
2022-02-05 09:00:06 +0700

    Change input from floating to default and Change position form to center viewport vertically

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -1,55 +1,79 @@
 @extends('auth.layouts.main')
 
 @section('container')
+    <div class="row justify-content-center align-items-center vh-100">
+        <div class="col-md-4">
+            @if (session()->has('success'))
+                <div class="alert alert-success alert-dismissible fade show"
+                    role="alert">
+                    {{ session('success') }}
+                    <button type="button"
+                        class="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="Close"></button>
+                </div>
+            @endif
 
-<div class="row justify-content-center col-12">
-    <div class="col-md-4">
-        @if (session()->has('success'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
+            @if (session()->has('loginError'))
+                <div class="alert alert-danger alert-dismissible fade show"
+                    role="alert">
+                    {{ session('loginError') }}
+                    <button type="button"
+                        class="btn-close"
+                        data-bs-dismiss="alert"
+                        aria-label="Close"></button>
+                </div>
+            @endif
 
-        @if (session()->has('loginError'))
-        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('loginError') }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
+            <main class="form-signin">
+                <h3 class="mb-4 text-center">Login</h3>
+                <form action="/login"
+                    method="post">
+                    @csrf
+                    <div class="mb-3">
+                        <label class="form-label"
+                            for="email">Email</label>
+                        <input type="email"
+                            class="form-control @error('email') is-invalid @enderror"
+                            name="email"
+                            id="email"
+                            autofocus
+                            required
+                            value="{{ old('email') }}">
 
-        <main class="form-signin">
-            <h1 class="h3 mb-3 fw-normal text-center">Login</h1>
-            <form action="/login" method="post">
-                @csrf
-                <div class="form-floating">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                        id="email" placeholder="name@example.com" autofocus required value="{{ old('email') }}">
-                    <label for="email">Email address</label>
-                    @error('email')
-                    <div class="invalid-feedback">
-                        {{ $message }}
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
-                    @enderror
-                </div>
-                <div class="form-floating mb-2">
-                    <input type="password" class="form-control" name="password" id="password" placeholder="Password"
-                        required>
-                    <label for="password">Password</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" name="remember_me" type="checkbox" value="true" id="remember_me"
-                        @if(old('remember_me')) checked @endif>
-                    <label class="form-check-label" for="remember_me">
-                        Remember me
-                    </label>
-                </div>
-                <button class="w-100 btn btn-lg mt-3 btn-primary" type="submit">Login</button>
-                <small class="d-block text-center mt-3">Not Registered? <a class="text-decoration-none"
-                        href="/register">Register</a></small>
-            </form>
-        </main>
+                    <div class="mb-2">
+                        <label class="form-label"
+                            for="password">Password</label>
+                        <input type="password"
+                            class="form-control"
+                            name="password"
+                            id="password"
+                            required>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input"
+                            name="remember_me"
+                            type="checkbox"
+                            value="true"
+                            id="remember_me"
+                            @if (old('remember_me')) checked @endif>
+                        <label class="form-check-label"
+                            for="remember_me">
+                            Remember me
+                        </label>
+                    </div>
+                    <button class="w-100 btn btn-lg mt-3 mb-4 btn-primary"
+                        type="submit">Login</button>
+                    <small class="d-block text-center">Not Registered? <a class="text-decoration-none"
+                            href="/register">Register</a></small>
+                </form>
+            </main>
+        </div>
     </div>
-</div>
-
 @endsection
