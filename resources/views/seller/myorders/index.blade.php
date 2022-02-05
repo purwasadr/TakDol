41dbaafe8d965e1b3586866d7fe8f422ebd87b93
Purwa Sadr <purwasadr@gmail.com>
2022-02-05 08:56:15 +0700

    Add form and change header list

--- resources/views/seller/myorders/index.blade.php
+++ resources/views/seller/myorders/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 
-<h2 class="mt-4 mb-4">My Orders</h2>
+<h4 class="mt-4 mb-4">My Orders</h4>
 
 <div class="list-group mb-4">
     <div class="list-group-item bg-light">
@@ -10,10 +10,10 @@
             <div class="col-1">
                 <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
             </div>
-            <div class="col-4">Product</div>
-            <div class="col-3">Price</div>
+            <div class="col-3">Product</div>
+            <div class="col-3">Status</div>
             <div class="col-3">Buyer</div>
-            <div class="col-1">Action</div>
+            <div class="col-2">Action</div>
         </div>
     </div>
 
@@ -24,12 +24,31 @@
             <div class="col-1"><input class="form-check-input" type="checkbox" id="checkboxNoLabel" value=""
                     aria-label="...">
             </div>
-            <div class="col-4">
+            <div class="col-3">
                 {{ $order->product->title }}
             </div>
-            <div class="col-3">Rp. {{ number_format($order->product->price, 0, ",", ".") }}</div>
+            <div class="col-3">{{ $order->status->name }}</div>
             <div class="col-3">{{ $order->buyer->name }}</div>
-            <div class="col-1">Delete</div>
+            <div class="col-2 d-inline">
+
+                <form action="/seller/myorders/edit-status" method="POST" class="d-inline">
+                    @csrf
+                    <input type="hidden" name="order_id" value="{{ $order->id }}">
+                    @switch($order->status->id)
+                    @case(1)
+                    <input type="hidden" name="status" value="2">
+                    <button type="submit" class="btn btn-sm text-decoration-none">Process</button>
+                    @break
+                    @case(2)
+                    <input type="hidden" name="status" value="3">
+                    <button type="submit" class="btn btn-sm text-decoration-none">On Ship</button>
+                    @default
+
+                    @endswitch
+                </form>
+                {{-- <a href="" class="text-decoration-none">Process</a> --}}
+                <a href="" class="text-decoration-none">Cancel</a>
+            </div>
         </div>
     </div>
     @endforeach
