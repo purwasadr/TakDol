318fdc1d0778b13b704664eec8f5845b5310c13b
Purwa Sadr <purwasadr@gmail.com>
2022-02-09 17:27:07 +0700

    Fix column store_name must nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique();
-            $table->string('store_name');
+            $table->string('store_name')->nullable();
             $table->string('profile_img')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
