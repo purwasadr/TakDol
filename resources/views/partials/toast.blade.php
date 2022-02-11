7f730afef864c952738003985514aa59fb31c16f
Purwa Sadr <purwasadr@gmail.com>
2022-02-11 07:48:09 +0700

    Add toast blade view

---
+++ resources/views/partials/toast.blade.php
@@ -0,0 +1,18 @@
+<div aria-live="polite"
+    aria-atomic="true"
+    class="position-fixed start-50 p-3"
+    style="transform: translate(-50%, 0); bottom: 11%">
+
+    <div id="toast"
+        class="toast text-white bg-dark border-0; z-index: 11;"
+        role="alert"
+        data-bs-delay="2000">
+        <div class="d-flex">
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+            </div>
+        </div>
+    </div>
+
+
+</div>
